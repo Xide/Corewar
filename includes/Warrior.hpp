364edaa75ed9989148811b37556745ac5f93d952
Germain GAU <germain.gau@gmail.com>
2015-07-12 15:43:40 +0200

    Appending warrior container

---
+++ includes/Warrior.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+#include "Instruction.hpp"
+
+class Warrior {
+  public:
+    Warrior(std::string const& _name,
+            std::string const& _author = "",
+            std::string const& _version = "",
+            std::string const& _date = "");
+
+    const std::string                 name;
+    const std::string                 author;
+    const std::string                 version;
+    const std::string                 date;
+    std::vector<Instruction>          source;
+
+    /// followings operators are used by scheduler to
+    /// deal with pendings tasks for a warrior
+    unsigned                    operator++();
+    unsigned                    operator++(int);
+    unsigned                    operator--();
+    unsigned                    operator--(int);
+    unsigned                    operator*() const;
+  private:
+    unsigned                    _pendingTasks;
+};
+
+std::ostream&                   operator<<(std::ostream&, Warrior const&);
---
+++ sources/common/Warrior.cpp
@@ -0,0 +1,37 @@
+#include "Warrior.hpp"
+
+Warrior::Warrior(std::string const& _name,
+                 std::string const& _author,
+                 std::string const& _version,
+                 std::string const& _date) :
+  name(_name), author(_author), version(_version), date(_date) {}
+
+unsigned
+Warrior::operator++() { return ++_pendingTasks; }
+
+unsigned
+Warrior::operator++(int) { return _pendingTasks++; }
+
+unsigned
+Warrior::operator--() {
+    if (_pendingTasks == 0)
+      throw std::logic_error("trying to decrement an already empty warrior task.");
+    return --_pendingTasks;
+}
+
+unsigned
+Warrior::operator--(int) {
+    if (_pendingTasks == 0)
+      throw std::logic_error("trying to decrement an already empty warrior task.");
+    return _pendingTasks--;
+}
+
+unsigned
+Warrior::operator*(void) const {
+     return _pendingTasks;
+}
+
+std::ostream&
+operator<<(std::ostream& os, Warrior const& w) {
+  return (os << "<Warrior " << w.name << " at " << &w << ">");
+}
