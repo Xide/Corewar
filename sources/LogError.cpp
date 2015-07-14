69ea6de165a35ff8548ef00f67bf38e18905c8dc
SiN <amaury.ravanel@epitech.eu>
2015-07-14 18:36:27 +0200

    SiN: add the execptions for the logger

---
+++ sources/LogError.cpp
@@ -0,0 +1,16 @@
+//
+// Author: SiN
+// Project: Corewar
+// Avaible: https://github.com/Xide/Corewar.git
+//
+
+#include "LogError.hpp"
+
+LogError::LogError(const std::string &name) throw() {
+    _what = name;
+}
+
+const char *
+LogError::what() const throw() {
+    return _what.c_str();
+}
