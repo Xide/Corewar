56ba2715cb4ad8ed5f6d9879346689d600528ccd
aslafy_z <aslafy_z@epitech.eu>
2015-07-15 16:57:30 +0200

    norme: hide uncrustify config / rewrite normit.sh

---
+++ normit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+function process {
+    if [ -f $1 ] ; then
+	uncrustify -c .uncrustify.cfg --replace --no-backup $1
+    fi
+}
+
+for file in */{sources,includes}/*.{cpp,h*};
+do
+    if grep -q '*' <<< $file; then continue; fi
+    process $file
+done
